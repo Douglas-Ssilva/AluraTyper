64d98576fd059b7971d644fc5358d5e8a2b8ff1a
Douglas Silva <douglas_d1994@hotmail.com>
2019-12-18 00:20:21 -0300

    Commit

--- udemy/js/script.js
+++ udemy/js/script.js
@@ -3,10 +3,8 @@ $('.sub th').prepend('<img src="img/add.png" class="img">');
 $('img').bind('click', function(){
     var valueSrcImg= $(this).attr('src');
     if(valueSrcImg === 'img/remove.png'){
-        //$(this).attr('src', 'img/add.png').parents().siblings('tr').hide();
         $(this).attr('src', 'img/add.png').parents().nextUntil("tr.sub").hide(); 
     }else{
-        //$(this).attr('src', 'img/remove.png').parents().siblings('tr').show();
         $(this).attr('src', 'img/remove.png').parents().nextUntil("tr.sub").show()
     }
 });
