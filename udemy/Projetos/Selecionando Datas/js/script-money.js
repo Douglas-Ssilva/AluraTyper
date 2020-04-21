181bda10ba9dbb6f437f2e635ccd49a349bd091b
Douglas Silva <douglas_d1994@hotmail.com>
2020-04-21 14:13:29 -0300

    Commit

--- udemy/Projetos/Selecionando Datas/js/script-money.js
+++ udemy/Projetos/Selecionando Datas/js/script-money.js
@@ -19,5 +19,5 @@ function getTotalInputs(){
 function numberToReal(numero) {
     var numero = numero.toFixed(2).split('.');
     numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
-    return numero.join(',');
+    return numero.join(','); /**junta todos elementos de um array e retorna uma string */
 }
